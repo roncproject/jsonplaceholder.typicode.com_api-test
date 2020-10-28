8b369456e439f7ee0125b7c75fe5c0ff882e7fd8
ron c project <ron.c.project@gmail.com>
2020-10-28 10:33:38 +0100

    Loglevel INFO 2 TRACE 4 steps

--- src/test/java/com/ysshha/apitest/StepDefinitions.java
+++ src/test/java/com/ysshha/apitest/StepDefinitions.java
@@ -20,91 +20,92 @@ public class StepDefinitions {
 
     @Before
     public void setUp() throws Throwable {
-		logger.info("StepDefinitions.setUp()");
 
-			listCall = new ListCall();
-			listCall.initCalls("http://localhost:8080");
-			createCall = new CreateCall();
-			createCall.initCalls("http://localhost:8080");
-			idCall = new IdCall();
-			idCall.initCalls("http://localhost:8080");
+		listCall = new ListCall();
+		listCall.initCalls("http://localhost:8080");
+		createCall = new CreateCall();
+		createCall.initCalls("http://localhost:8080");
+		idCall = new IdCall();
+		idCall.initCalls("http://localhost:8080");
 
+		logger.trace("StepDefinitions.setUp()");
 	}
 	
 	@Given("I request a List")
 	public void I_request_a_List() {
-		logger.info("StepDefinitions.I_request_a_List()");
 
 		listCall.getList();
+
+		logger.trace("StepDefinitions.I_request_a_List()");
 	}
 
 	@When("I have a correct response")
 	public void I_have_a_correct_response() {
-		logger.info("StepDefinitions.I_have_a_correct_response()");
 
 		listCall.checkList();
+
+		logger.trace("StepDefinitions.I_have_a_correct_response()");
     }
 
 
 	@Then("There are {int} orders")
 	public void There_are_int_orders(int i) {
-		logger.info("StepDefinitions.There_are_int_orders()");
 
 		listCall.numOrdersInList(i);
 
+		logger.trace("StepDefinitions.There_are_int_orders()");
 	}
+
 
 	@And("I see order {int} with card {string} pass {string} status {string} and amount {int}")
 	public void There_are_int_orders(int i1, String s1, String s2, String s3, int i2) {
-    	logger.info("StepDefinitions.There_are_int_orders()");
 
 		listCall.orderInList(i1, s1, s2, s3, i2);
+
+		logger.trace("StepDefinitions.There_are_int_orders()");
 	}
 
-//	@When("I create an Order")
-//	public void I_create_an_Order() {
-//		logger.info("StepDefinitions.I_create_an_Order()");
-//
-//    	createCall.doCreate(1000, "202", "303");
-//	}
 
 	@Given("I create an Order with amount {int} card {string} pass {string}")
 	public void I_create_an_Order_with(int i, String s1, String s2) {
-		logger.info("StepDefinitions.I_create_an_Order_with()");
 
 		createCall.doCreate(i, s1, s2);
+
+		logger.trace("StepDefinitions.I_create_an_Order_with()");
 	}
 
 
 
 	@When("The order is Ok")
 	public void The_order_is_Ok() {
-		logger.info("StepDefinitions.The_order_is_Ok()");
 
 		createCall.checkCreate();
+
+		logger.trace("StepDefinitions.The_order_is_Ok()");
 	}
 
 
 	@Given("I search an Order {int}")
 	public void I_search_an_Order(int i) {
-		logger.info("StepDefinitions.I_search_an_Order(" + i + ")");
 
 		idCall.getId(i);
+
+		logger.trace("StepDefinitions.I_search_an_Order(" + i + ")");
 	}
 
 	@When("The order is correct")
 	public void The_order_is_Correct() {
-		logger.info("StepDefinitions.The_order_is_Correct()");
 
 		createCall.checkCreate();
+
+		logger.trace("StepDefinitions.The_order_is_Correct()");
 	}
 
 
 	@After
     public void tearDown() throws Throwable {
-		logger.info("StepDefinitions.tearDown()");
-
-		//orderPage.closePage();
+		logger.trace("StepDefinitions.tearDown()");
+		
 	}
 
 }
